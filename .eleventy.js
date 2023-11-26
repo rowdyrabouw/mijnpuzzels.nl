4d887ac21fe240e86ea940c0023556b16d13ae62
Rowdy Rabouw <rowdy@double-r.nl>
2023-11-26 11:58:49 +0100

    feat: initial commit

---
+++ .eleventy.js
@@ -0,0 +1,44 @@
+// Pull in the environment variables. In dev, this comes from the `.env` file.
+// In prod, this comes from Netlify
+require("dotenv").config();
+
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const embedCodePen = require("@manustays/eleventy-plugin-codepen-iframe");
+
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  // https://github.com/manustays/eleventy-plugin-codepen-iframe
+  eleventyConfig.addPlugin(embedCodePen, {
+    tabs: "js,result",
+    user: "rowdy"
+  });
+
+  // eleventyConfig.addPassthroughCopy("src/.htaccess");
+  // eleventyConfig.addPassthroughCopy("src/*.txt");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  // eleventyConfig.addPassthroughCopy({ "src/icons": "/" });
+  eleventyConfig.addPassthroughCopy({ "src/txt": "/" });
+  eleventyConfig.addPassthroughCopy({ "src/favicon": "/" });
+
+  eleventyConfig.addFilter("toCanonicalUrl", (url) => {
+    if (url.slice(-1) === "/") {
+      return url;
+    } else {
+      return `${url}/`;
+    }
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "dist",
+      templateFormats: ["md", "njk"],
+      passthroughFileCopy: true,
+    },
+  };
+};
---
+++ src/_data/settings.js
@@ -0,0 +1,8 @@
+
+
+module.exports = () => {
+  return {
+    environment: process.env.NODE_ENV || "PRD",
+    now: new Date(),
+  };
+};
